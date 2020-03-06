803f55ca1a8f671caac71d5d58505a66a94c2b77
Shikher Pant <55846733+shikherpant@users.noreply.github.com>
2020-03-06 13:39:08 +0530

    Add files via upload

---
+++ AR Greeting Card/Resources/RotateSphere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSphere : MonoBehaviour
+{
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3 (0, 30, 0);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(movement * Time.deltaTime);
+    }
+}
